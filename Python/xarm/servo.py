8e3a27a83974891dcbd81b819ba05aef566f9a7e
Nate Woods <big.nate.w@gmail.com>
2022-11-16 10:34:22 +0000

    Pending changes exported from your codespace

--- Python/xarm/servo.py
+++ Python/xarm/servo.py
@@ -1,27 +1,14 @@
 from .util import Util
 
 class Servo:
-    def __init__(self, servo_id, position=500):
+    def __init__(self, servo_id, initial=500, minimum=500, maximum=2500):
         self.servo_id = servo_id
+        self.__value = initial
 
-        if isinstance(position, int):
-            self.__set_position(position)
-        elif isinstance(position, float):
-            self.__set_angle(position)
-    
-    def __get_position(self):
-        return self.__position
+    def __get_value(self):
+        return self.__value  # do we even care about casting?
 
-    def __set_position(self, position):
-        self.__position = int(position)
-        self.__angle = Util._position_to_angle(self.__position)
+    def __set_value(self, value):
+        self.__value = value
 
-    def __get_angle(self):
-        return self.__angle
-
-    def __set_angle(self, degrees):
-        self.__angle = float(degrees)
-        self.__position = Util._angle_to_position(self.__angle)
-
-    position = property(__get_position, __set_position)
-    angle = property(__get_angle, __set_angle)
+    value = property(__get_value, __set_value)
